ddcfa66c6e5dc4f724fff294f02f0217dbd9c460
Renan Delalibera Gomes <renan.gomes@ayoshii.com.br>
2024-11-13 10:04:28 -0300

    atualização controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,13 +1,10 @@
-// controllers/usuarioController.js
 const { User } = require('../models');
-const readlineSync = require('readline-sync'); // Importa readline-sync para capturar a entrada do usuário
+const readlineSync = require('readline-sync');
 
 async function adicionarUser(transaction) {
-  // Captura os dados do usuário através da linha de comando
   const nome = readlineSync.question('Digite o nome do usuario: ');
   const email = readlineSync.question('Digite o email do usuario: ');
 
-  // Criar o usuário na transação
   return User.create({
     nome: nome,
     email: email
@@ -15,16 +12,15 @@ async function adicionarUser(transaction) {
 }
 
 async function editarUser(transaction) {
-  // Captura o email do usuário para edição
-  const email = readlineSync.question('Digite o email do usuario a ser inserido: ');
+  const email = readlineSync.question('Digite o email do usuario a ser editado: ');
 
-  // Simula a edição do usuário
   const user = await User.findOne({ where: { email: email } });
-  
+
   if (user) {
-    const novoNome = readlineSync.question('Digite o novo nome do usuario: '); // Captura o novo nome
+    const novoNome = readlineSync.question('Digite o novo nome do usuario: ');
     user.nome = novoNome;
     await user.save({ transaction });
+    console.log('Usuário atualizado com sucesso!');
   } else {
     console.log('Usuario nao encontrado!');
   }
@@ -33,30 +29,37 @@ async function editarUser(transaction) {
 module.exports = {
   realizarTransacao: async () => {
     const sequelize = require('../models').sequelize;
-
     const transaction = await sequelize.transaction();
 
     try {
-      // Realizando ações dentro da transação
-      await adicionarUser(transaction);
-      await editarUser(transaction);
+      const acao = readlineSync.question('Deseja inserir um novo usuário ou editar um existente? (inserir/editar): ');
 
-      // Pergunta ao usuário se ele deseja confirmar ou desfazer a transação
-      const acao = readlineSync.question('Deseja confirmar a transacaoo (commit) ou desfazer (rollback)? (commit/rollback): ');
-
-      if (acao === 'commit') {
-        await transaction.commit();
-        console.log('Transacao confirmada!');
-      } else if (acao === 'rollback') {
-        await transaction.rollback();
-        console.log('Transacao desfeita!');
+      if (acao === 'inserir') {
+        await adicionarUser(transaction);
+        console.log('Usuário inserido com sucesso!');
+      } else if (acao === 'editar') {
+        await editarUser(transaction);
       } else {
-        console.log('Acao invalida! A transacao sera desfeita.');
+        console.log('Ação inválida! A transação será desfeita.');
+        await transaction.rollback();
+        return;
+      }
+
+      const confirmacao = readlineSync.question('Deseja confirmar a transacao (commit) ou desfazer (rollback)? (commit/rollback): ');
+
+      if (confirmacao === 'commit') {
+        await transaction.commit();
+        console.log('Transação confirmada!');
+      } else if (confirmacao === 'rollback') {
+        await transaction.rollback();
+        console.log('Transação desfeita!');
+      } else {
+        console.log('Ação inválida! A transação será desfeita.');
         await transaction.rollback();
       }
     } catch (error) {
       await transaction.rollback();
-      console.error('Erro na transacao:', error);
+      console.error('Erro na transação:', error);
     }
   }
 };
